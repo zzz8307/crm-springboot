75f87850f1d3489187632dff4fb05a6f363fb3de
zzz8307 <zzz8307@gmail.com>
2022-03-01 23:27:28 +0800

    Make it a deployable WAR file.

--- src/main/java/com/rc/crm/CrmSpringbootApplication.java
+++ src/main/java/com/rc/crm/CrmSpringbootApplication.java
@@ -3,13 +3,19 @@ package com.rc.crm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author rc
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.rc.crm.*.dao")
-public class CrmSpringbootApplication {
+public class CrmSpringbootApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CrmSpringbootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CrmSpringbootApplication.class, args);
